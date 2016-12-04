2177179ce8f1d304da8b9df036fbf78030d858fe
TobiasEberharter <tobias_e2@hotmail.com>
2016-12-04 23:39:19 +0100

    Update initialization.hpp

--- initialization.hpp
+++ initialization.hpp
@@ -33,13 +33,15 @@ std::vector<individual> initialization_1(WindScenario wscenario,int pop_size);
  * constraints as parameters, and automatically corrects
  * all turbine positions within the given population by new
  * random positions. No return value.
+ * 
+ * !! srand(time(0)); needs to be in the main function !!
  *
  * parameters: std::vector<individual> &population
  * 			   double radius, int width, int height
  *
  * return: void
  */
-void replace_violations(std::vector<individual> &population,WindScenari wscenario);
+void replace_violations(std::vector<individual> &population,WindScenario wscenario);
 }
 
 
